0361a3b2af45ba61d3a38d941a090335b3666880
Maia Coghlan <maiaccog@gmail.com>
2019-05-04 18:13:46 +0800

    use prettier

--- script.js
+++ script.js
@@ -13,87 +13,87 @@ const sliderController = (() => {
 	}
 	const setNewIndex = n => {
 		if (n == slides.length) {
-      slideIndex = 0
-      nextIndex = 1
-      prevIndex = slides.length - 1
+			slideIndex = 0
+			nextIndex = 1
+			prevIndex = slides.length - 1
 		} else if (n < 0) {
-      slideIndex = slides.length - 1
-      nextIndex = 0
-      prevIndex = slideIndex - 1
+			slideIndex = slides.length - 1
+			nextIndex = 0
+			prevIndex = slideIndex - 1
 		} else {
-      slideIndex = n
-      if (slideIndex == 0) {
-        nextIndex = 1
-        prevIndex = slides.length - 1
-      } else if (slideIndex == slides.length - 1) {
-        nextIndex = 0
-        prevIndex = slideIndex - 1
-      } else {
-        nextIndex = slideIndex + 1
-        prevIndex = slideIndex - 1
-      }
+			slideIndex = n
+			if (slideIndex == 0) {
+				nextIndex = 1
+				prevIndex = slides.length - 1
+			} else if (slideIndex == slides.length - 1) {
+				nextIndex = 0
+				prevIndex = slideIndex - 1
+			} else {
+				nextIndex = slideIndex + 1
+				prevIndex = slideIndex - 1
+			}
 		}
 	}
 	const showNewSlide = () => {
-    slides.forEach((val, num) => {
-      if (num == prevIndex) {
-        slides[num].className = "mySlides prev-slide"
-      } else if (num == slideIndex) {
-        slides[num].className = "mySlides current-slide"
-      } else if (num == nextIndex) {
-        slides[num].className = "mySlides next-slide"
-      } else {
-        slides[num].className = "mySlides"
-      }
-    })
-    dots[slideIndex].className = 'dot active'
+		slides.forEach((val, num) => {
+			if (num == prevIndex) {
+				slides[num].className = 'mySlides prev-slide'
+			} else if (num == slideIndex) {
+				slides[num].className = 'mySlides current-slide'
+			} else if (num == nextIndex) {
+				slides[num].className = 'mySlides next-slide'
+			} else {
+				slides[num].className = 'mySlides'
+			}
+		})
+		dots[slideIndex].className = 'dot active'
 	}
 	const goToSlide = n => {
-    dots[slideIndex].className = 'dot'
+		dots[slideIndex].className = 'dot'
 		setNewIndex(n)
 		showNewSlide()
-  }
-  const resetPrevNext = () => {
-    slides.forEach((val, num) => {
-      if (num == prevIndex) {
-        slides[num].className = "mySlides prev-slide behind"
-      } else if (num == slideIndex) {
-        slides[num].className = "mySlides current-slide"
-      } else if (num == nextIndex) {
-        slides[num].className = "mySlides next-slide behind"
-      } else {
-        slides[num].className = "mySlides"
-      }
-    })
-    removeBehind()
-  }
-  const removeBehind = () => {
-    slides.forEach((val, num) => {
-      if (num == prevIndex) {
-        slides[num].className = "mySlides prev-slide"
-      } else if (num == nextIndex) {
-        slides[num].className = "mySlides next-slide"
-      }
-    })
-  }
-  const dotsNav = n => {
-    if (slides[n].className === "mySlides") {
-      dots[slideIndex].className = 'dot'
-      slides.forEach((val, num) => {
-        if (num == slideIndex) {
-          slides[num].className = "mySlides fade"
-        } else {
-          slides[num].className = "mySlides"
-        }
-      })
-      slides[n].className = "mySlides current-slide fade"
-      dots[n].className = 'dot active'
-      setNewIndex(n)
-      setTimeout(resetPrevNext, 1000)
-    } else {
-      goToSlide(n)
-    }
-  }
+	}
+	const resetPrevNext = () => {
+		slides.forEach((val, num) => {
+			if (num == prevIndex) {
+				slides[num].className = 'mySlides prev-slide behind'
+			} else if (num == slideIndex) {
+				slides[num].className = 'mySlides current-slide'
+			} else if (num == nextIndex) {
+				slides[num].className = 'mySlides next-slide behind'
+			} else {
+				slides[num].className = 'mySlides'
+			}
+		})
+		removeBehind()
+	}
+	const removeBehind = () => {
+		slides.forEach((val, num) => {
+			if (num == prevIndex) {
+				slides[num].className = 'mySlides prev-slide'
+			} else if (num == nextIndex) {
+				slides[num].className = 'mySlides next-slide'
+			}
+		})
+	}
+	const dotsNav = n => {
+		if (slides[n].className === 'mySlides') {
+			dots[slideIndex].className = 'dot'
+			slides.forEach((val, num) => {
+				if (num == slideIndex) {
+					slides[num].className = 'mySlides fade'
+				} else {
+					slides[num].className = 'mySlides'
+				}
+			})
+			slides[n].className = 'mySlides current-slide fade'
+			dots[n].className = 'dot active'
+			setNewIndex(n)
+			setTimeout(resetPrevNext, 1000)
+		} else {
+			goToSlide(n)
+		}
+	}
 	const display = clicked => {
 		clearInterval(slideInterval)
 		if (clicked.className == 'prev') {
@@ -101,7 +101,7 @@ const sliderController = (() => {
 		} else if (clicked.className == 'next') {
 			toNextSlide()
 		} else if (clicked.className == 'dot') {
-      dotsNav(dots.indexOf(clicked))
+			dotsNav(dots.indexOf(clicked))
 		}
 		slideInterval = setInterval(toNextSlide, 40000)
 	}
